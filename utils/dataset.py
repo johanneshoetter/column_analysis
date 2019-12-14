77fec5861651281e767cf5b972a1bb5f70a1e6f5
johanneshoetter <57453680+johanneshoetter@users.noreply.github.com>
2019-12-14 14:00:32 +0100

    Added padding, currently not working as tensors cant be build from list of tensors

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,8 +1,9 @@
 import torch
+from torch.utils import data
 import os
 
 
-class ExtendedWikiSQL():
+class ExtendedWikiSQL(data.Dataset):
     
     def __init__(self):
         self.inputs, self.targets = [], []
@@ -26,10 +27,12 @@ class ExtendedWikiSQL():
             input_suf = [embedding(word.lower()) for word in header]
             input_sequence = [embedding('<BEG>')] + input_pre + [embedding('<SEQ>')] + input_suf + [embedding('<END>')]
 
-            target = [float(val) for val in target]
-
+            target_pre = [0 for word in question] # question
+            target_suf = [1 if indicator == '1' else 0 for indicator in target] # header
+            target_sequence = [0] + target_pre + [0] + target_suf + [0]
+            
             self.inputs.append(torch.Tensor(input_sequence))
-            self.targets.append(torch.Tensor(target))
+            self.targets.append(torch.LongTensor(target_sequence))
                 
     def load_from_torch(self, path):
         self.inputs = torch.load('{}_inputs.pt'.format(path))
